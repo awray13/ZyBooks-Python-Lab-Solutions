7475490b08d4f566afc2cff625cb8919d394abce
Ashton Wray <awray11@wgu.edu>
2024-07-30 20:00:45 -0400

    Fixed code to grant appropriate permissions to admin and regular users.

--- zybooks_q24.py
+++ zybooks_q24.py
@@ -11,69 +11,63 @@ You will need to fix the grant permission method.
 import os
 import stat
 
-def admin(filename, admin):
-    return admin
+def admin(filename):
+    # Admins get full permissions: read, write, execute
+    os.chmod(filename, stat.S_IRWXU)
     
-def user(filename, user):
-    return user    
+
+def user(filename):
+    # Users get read permissions only
+    os.chmod(filename, stat.S_IRUSR)
     
-def grant_permission(name_list, filename):
-    # FIXME - Fix the permission level for each user
-    for name in name_list:
-        if admin(filename, name):
-            os.chmod(filename, stat.S_IRWXU)
-        elif user(filename, name):
-            os.chmod(filename, stat.S_IRUSR)
+
+def grant_permission(is_admin, filename):
+    if is_admin:
+        admin(filename)
+    else:
+        user(filename)
 
     check_permission(filename)
 
-    return True
-
 def check_permission(filename):
     # Check if file path exists
-    path1 = os.access(filename, os.F_OK)
-    print("The path exists:", path1)
-    # Check if User has Read Access
-    path2 = os.access(filename, os.R_OK)
-    print("Access to read the file:", path2)
-    # Check if User has Write Access
-    path3 = os.access(filename, os.W_OK)
-    print("Access to write the file:", path3)
-    # Check if User has Execute Permission
-    path4 = os.access(filename, os.X_OK)
-    print("Check if path can be executed:", path4)
-
-    if os.access(filename, os.R_OK):
-    	# open txt file as file
-    	with open(filename) as file:
-    		file.read()
-            
-    else:
-        #in case can't access the file	
-        print("Cannot access the file")
-        with open("output_file.txt", 'w') as f:
-            if os.access(filename, os.W_OK):
-                f.write("I have write privilege.\n")
+    path_exists = os.access(filename, os.F_OK)
+    print("The path exists:", path_exists)
     
+    # Check if User has Read Access
+    read_access = os.access(filename, os.R_OK)
+    print("Access to read the file:", read_access)
+    
+    # Check if User has Write Access
+    write_access = os.access(filename, os.W_OK)
+    print("Access to write the file:", write_access)
+    
+    # Check if User has Execute Permission
+    execute_access = os.access(filename, os.X_OK)
+    print("Check if path can be executed:", execute_access)
+
+    if read_access:
+        # Open and read the file
+        with open(filename) as file:
+            file.read()
+    else:        
+        # In case can't access the file    
+        print("Cannot access the file")
+
+    with open("output_file.txt", 'w') as f:
+        if write_access:
+            f.write("I have write privilege.\n")
+
 if __name__ == '__main__':
     filename = input()
     name = input()
-    k = []
-    result = False
+    user_list = []
 
     with open(filename) as f:
         for line in f:
             fields = line.split()
-            k.append(fields[0])
+            user_list.append(fields[0])
             
-        for lineNum in range(len(k)):
-            if name in k[lineNum]:
-                result = True
-                name_result = name
-                
-        if result:
-            admin(filename, name)
-        else:
-            user(filename, name)
+        is_admin = name in user_list
 
-    grant_permission(k, filename)
+    grant_permission(is_admin, filename)
